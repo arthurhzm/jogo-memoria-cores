6e37f89afba500a8159f48ee14c8be2a0b823cf3
Arthur Marena <arthurzhm2@gmail.com>
2024-12-05 07:20:41 -0300

    FIX: removendo a biblioteca de áudio

--- genius_app/lib/main.dart
+++ genius_app/lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+// import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const GeniusGame());
 }
 
 class GeniusGame extends StatelessWidget {
-  const GeniusGame({Key? key}) : super(key: key);
+  const GeniusGame({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: GeniusHome(),
     );
@@ -17,6 +18,8 @@ class GeniusGame extends StatelessWidget {
 }
 
 class GeniusHome extends StatefulWidget {
+  const GeniusHome({super.key});
+
   @override
   _GeniusHomeState createState() => _GeniusHomeState();
 }
@@ -31,6 +34,8 @@ class _GeniusHomeState extends State<GeniusHome> {
 
   List<String> sequence = [];
   bool isUserTurn = false;
+  // final AudioPlayer audioPlayer = AudioPlayer();
+  int score = 0;
 
   void addRandomColor() {
     final random = activeColors.keys.toList()..shuffle();
@@ -47,6 +52,8 @@ class _GeniusHomeState extends State<GeniusHome> {
   }
 
   Future<void> _activateColor(String color) async {
+    // await audioPlayer.play(AssetSource('assets/$color.mp3'));
+
     setState(() {
       activeColors[color] = true;
     });
@@ -63,74 +70,79 @@ class _GeniusHomeState extends State<GeniusHome> {
     playSequence();
   }
 
+  void updateScore() {
+    setState(() {
+      score = sequence.length - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Genius Game'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 300,
-              height: 300,
-              child: GridView.count(
-                crossAxisCount: 2,
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 8,
-                children: [
-                  GeniusButton(
-                    color: activeColors['red']! ? Colors.redAccent : Colors.red,
-                    onTap: () {
-                      if (isUserTurn) {
-                        _activateColor('red');
-                        _checkColor('red');
-                      }
-                    },
-                  ),
-                  GeniusButton(
-                    color: activeColors['green']!
-                        ? Colors.lightGreen
-                        : Colors.green,
-                    onTap: () {
-                      if (isUserTurn) {
-                        _activateColor('green');
-                        _checkColor('green');
-                      }
-                    },
-                  ),
-                  GeniusButton(
-                    color:
-                        activeColors['blue']! ? Colors.lightBlue : Colors.blue,
-                    onTap: () {
-                      if (isUserTurn) {
-                        _activateColor('blue');
-                        _checkColor('blue');
-                      }
-                    },
-                  ),
-                  GeniusButton(
-                    color: activeColors['yellow']!
-                        ? Colors.yellowAccent
-                        : Colors.yellow,
-                    onTap: () {
-                      if (isUserTurn) {
-                        _activateColor('yellow');
-                        _checkColor('yellow');
-                      }
-                    },
-                  ),
-                ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Pontuação: $score',
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Container(
+                width: 300,
+                height: 300,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  children: [
+                    GeniusButton(
+                      color: activeColors['green']!
+                          ? Colors.lightGreen
+                          : Colors.green,
+                      onTap: () {
+                        if (isUserTurn) checkUserInput('green');
+                      },
+                    ),
+                    GeniusButton(
+                      color:
+                          activeColors['red']! ? Colors.redAccent : Colors.red,
+                      onTap: () {
+                        if (isUserTurn) checkUserInput('red');
+                      },
+                    ),
+                    GeniusButton(
+                      color: activeColors['yellow']!
+                          ? Colors.yellowAccent
+                          : Colors.yellow,
+                      onTap: () {
+                        if (isUserTurn) checkUserInput('yellow');
+                      },
+                    ),
+                    GeniusButton(
+                      color: activeColors['blue']!
+                          ? Colors.lightBlue
+                          : Colors.blue,
+                      onTap: () {
+                        if (isUserTurn) checkUserInput('blue');
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
-            ElevatedButton(
-              onPressed: startNewRound,
-              child: const Text('Iniciar'),
-            ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: startNewRound,
+            child: const Text('Iniciar'),
+          ),
+        ],
       ),
     );
   }
@@ -185,10 +197,10 @@ class GeniusButton extends StatelessWidget {
   final VoidCallback onTap;
 
   const GeniusButton({
-    Key? key,
+    super.key,
     required this.color,
     required this.onTap,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
